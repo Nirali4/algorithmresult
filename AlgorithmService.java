3d37562c9d19fee83b844492537f66a7fe24a6a3
yasho3115 <53729443+yasho3115@users.noreply.github.com>
2020-05-06 22:27:54 +1000

    Algorithm Results
    Faux Exchange Project
    Cryptocurrency

---
+++ AlgorithmService.java
@@ -0,0 +1,8 @@
+package com.g4.fauxexchange.service;
+
+import java.util.List ;
+import com.g4.fauxexchange.model.AlgorithmResult;
+
+public interface AlgorithmService {
+    public abstract void runAlgorithmResult(Algorithm algorithm);
+}
