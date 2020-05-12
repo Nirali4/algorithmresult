bf200b652d1348886361eff35ca89bc888867320
yasho3115 <53729443+yasho3115@users.noreply.github.com>
2020-05-12 17:14:13 +1000

    Delete AlgorithmService.java

--- AlgorithmService.java
+++
@@ -1,8 +0,0 @@
-package com.g4.fauxexchange.service;
-
-import java.util.List ;
-import com.g4.fauxexchange.model.AlgorithmResult;
-
-public interface AlgorithmService {
-    public abstract void runAlgorithmResult(Algorithm algorithm);
-}
